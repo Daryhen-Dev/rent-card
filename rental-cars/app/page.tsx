577afea6e90ab0af202f4777af02b3fe99539499
daryhen <daryhen@Proy.myguest.virtualbox.org>
2024-11-09 20:40:43 -0500

    first commit

---
+++ rental-cars/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hola in tailwind</p>
+      <Button>This is click</Button>
+    </div>
+  );
+}
